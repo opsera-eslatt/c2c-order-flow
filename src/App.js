0440c83629916f4b3f189c9fa632445e5a110e84
opsera-eslatt <131897365+opsera-eslatt@users.noreply.github.com>
2024-04-30 21:13:44 -0400

    conrives a delta for no-code pipeline video

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Code to Cloud
+          Fulfillment Portfolio - Code to Cloud - 2113
         </a>
       </header>
     </div>
